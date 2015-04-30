23e23de791411bb632080a8debc7a626ec558125
Michael Vescovo <s3459317@student.rmit.edu.au>
2015-04-30 17:18:20 +1000

    chromebook

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,8 +1,6 @@
 package application;
 
 
-import javax.swing.SwingUtilities;
-
 import controller.Controller;
 import view.MainFrame;
 import model.GameEngineImpl;
@@ -11,24 +9,15 @@ import model.interfaces.GameEngine;
 //Assignment 2 version
 public class Main {
 	public static void main(String[] args) {
-		final GameEngine gameEngine;
-		
-		
+		GameEngine gameEngine;
+		MainFrame mainFrame;
 		@SuppressWarnings("unused")
 		Controller controller;
 		
 		// MVC model. Controller has references to view and model.
 		// View has reference to model, and model has a callback to update the view.
 		gameEngine = new GameEngineImpl();
-		// create GUI from event dispatch thread
-		SwingUtilities.invokeLater(new Runnable() {
-			final MainFrame mainFrame;
-			public void run() {
-
-				mainFrame = new MainFrame(gameEngine);
-			}
-		});
-		
+		mainFrame = new MainFrame(gameEngine);
 		controller = new Controller(gameEngine, mainFrame);
 	}
 }
